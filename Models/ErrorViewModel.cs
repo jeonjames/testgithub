738caf0a1bfc539ebab50fc36396beaa34eb1fdf
jeonjames <56912254+jeonjames@users.noreply.github.com>
2019-12-10 16:09:09 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace testgithub.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
